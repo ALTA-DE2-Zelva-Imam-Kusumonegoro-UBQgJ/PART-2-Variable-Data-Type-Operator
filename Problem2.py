82286e8db62fd6f3d174b78a14cfbde36b0c637a
zelvaiz <zelvaimam15@gmail.com>
2024-02-15 13:54:30 +0700

    Edited for typo and formatting

--- Problem2.py
+++ Problem2.py
@@ -3,7 +3,7 @@ alas = float(input("alas (cm): "))
 tinggi = float(input("tinggi (cm): "))
 
 # Menghitung luas segitiga
-Luas_segitiga= alas * tinggi/2
+Luas_segitiga = alas * tinggi/2
 
 # Menampilkan hasil
 print(f"Luas segitiga adalah {Luas_segitiga} cm^2.")
